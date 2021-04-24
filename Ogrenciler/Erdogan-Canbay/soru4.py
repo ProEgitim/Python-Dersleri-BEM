aa61ffd2a2ab209e317f9b2c8c13587dda3f31cd
Canbay-Erdogan <Canbay-Erdogan@users.noreply.github.com>
2021-04-24 20:53:19 +0300

    liste calisma

---
+++ Ogrenciler/Erdogan-Canbay/soru4.py
@@ -0,0 +1,16 @@
+liste1=[1,2,3]
+print(liste1*3)
+
+liste2=[4,2,8,7,9,1]
+print(liste2)
+liste2.sort(reverse=True)
+print(liste2)
+
+liste3=[1,2,3]
+liste4=[4,5,6]
+liste5=[7,8,9]
+
+liste6=[liste3,liste4,liste5]
+print(liste6)
+
+
