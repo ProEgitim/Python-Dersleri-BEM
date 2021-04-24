9098cbb88d4a15d7fb741d719567874438cc5d43
Eren-123 <63911902+Eren-123@users.noreply.github.com>
2021-04-24 19:39:43 +0300

    Update soru3.py
    ufak düzenlemeler

--- Ogrenciler/Eren/soru3.py
+++ Ogrenciler/Eren/soru3.py
@@ -1,5 +1,10 @@
 a=int(input("1. Sayıyı giriniz : "))
 b=int(input("2. Sayıyı giriniz : "))
 sonuc=a,b = b,a
+print("----Yeni Hali------")
 print("a =",a)
 print("b =",b)
+
+print("-----Eski Hali-----")
+print("a =",b)
+print("b =",a)
