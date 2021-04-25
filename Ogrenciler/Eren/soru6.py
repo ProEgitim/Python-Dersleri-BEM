5de0fc1624c249cb21a0b9f59536c50570818bec
Eren-123 <63911902+Eren-123@users.noreply.github.com>
2021-04-25 04:22:00 +0300

    Create soru6.py
    soru 6 yapıldı

---
+++ Ogrenciler/Eren/soru6.py
@@ -0,0 +1,6 @@
+print("Dik Üçgen Hipotenüs Uygulaması")
+print("------------------------------")
+a = int(input("A uzunlugun Giriniz:"))
+b = int(input("B uzunlugunu Giriniz:")) 
+c = (a ** 2 + b ** 2) ** 0.5
+print("C Kenarı Hipotenus Uzunluğu :{}".format(c))
