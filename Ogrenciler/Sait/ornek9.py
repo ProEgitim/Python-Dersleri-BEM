05d8817b0f5df2049e3b571d72ab2458f88c7fee
saiterkam <67827592+saiterkam@users.noreply.github.com>
2021-04-25 19:10:04 +0300

    Create ornek9.py

---
+++ Ogrenciler/Sait/ornek9.py
@@ -0,0 +1,6 @@
+ilksinav= int(input("ilksinav:"))
+ikincisinav= int(input("ikincisinav:"))
+ucuncusinav= int(input("ucuncusinav:"))
+c=(ilksinav+ikincisinav+ucuncusinav)/3
+
+print("Ortalama notunuz:",c)
