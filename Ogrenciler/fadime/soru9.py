f2fa3b452b29aa63428ec1c87d1bfd94bfb5ffc8
111fadime <82760170+111fadime@users.noreply.github.com>
2021-04-25 19:07:13 +0300

    soru 9
    soru 9 in cevabı

---
+++ Ogrenciler/fadime/soru9.py
@@ -0,0 +1,5 @@
+birincisinav=int(input("lütfen birinci sinav sonunu giriniz"))
+ikincisinav=int(input("lütfen ikinci sinav sonunu giriniz"))
+ucuncusinav=int(input("lütfen ucuncu sinav sonunu giriniz"))
+c=(birincisinav+ikincisinav+ucuncusinav)/3
+print("ortalama notunuz:",c)
