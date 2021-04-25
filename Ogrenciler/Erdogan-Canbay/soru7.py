b6337400f6f94e5bae92e2b6af31e75d7f423113
Canbay-Erdogan <Canbay-Erdogan@users.noreply.github.com>
2021-04-25 18:46:19 +0300

    Soru7

---
+++ Ogrenciler/Erdogan-Canbay/soru7.py
@@ -0,0 +1,9 @@
+"""
+vize *0,3 final *0,7
+"""
+
+vize = int(input("vize notunuzu girin= "))
+final = int(input("final notunuzu girin= "))
+
+ortalama = (vize *30/100) + (final*70/100)
+print(ortalama)
