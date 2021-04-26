4ef2ec5ce75f83a786cfa41858dd364b78945040
ers1905 <ers1905@hotmail.com>
2021-04-26 10:21:36 +0300

    Create soru10.py
    soru 10 cevabı

---
+++ Ogrenciler/Ersan/soru10.py
@@ -0,0 +1,8 @@
+yaricap = input("Dairenin Yarıçapını Giriniz: ")
+ 
+cevre = ( 2 * 3.14 * float(yaricap) )
+ 
+alan = 3.14 * float(yaricap) * float(yaricap)
+ 
+print("\nDairenin Alanı : ",alan )
+print("\nDairenin Çevresi : ",cevre)
