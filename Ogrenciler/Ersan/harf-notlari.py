4f26266ebc8a3b1121cfac12e8ed0efe0cb3bdcc
ers1905 <ers1905@hotmail.com>
2021-05-02 20:24:12 +0300

    Update harf-notlari.py
    harf notları

--- Ogrenciler/Ersan/harf-notlari.py
+++ Ogrenciler/Ersan/harf-notlari.py
@@ -1,15 +1,15 @@
 notunuz =int(input("Lütfen yıl sonu not ortalamanızı giriniz:"))
 if (notunuz >=90):
     print("AA ile geçtiniz")
-elif(notunuz >85):
+elif(notunuz >=85):
     print("BA ile geçtiniz")
-elif(notunuz >80):
+elif(notunuz >=80):
     print("BB ile geçtiniz")
-elif(notunuz >75):
+elif(notunuz >=75):
     print("CB ile geçtiniz")
-elif(notunuz >65):
+elif(notunuz >=65):
     print("DC ile geçtiniz")
-elif(notunuz >55):
+elif(notunuz >=55):
     print("CC ile geçtiniz")
 else:
     print("FF ile kaldınız")
