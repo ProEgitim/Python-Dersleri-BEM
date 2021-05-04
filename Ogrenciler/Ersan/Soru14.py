d6f63d529c332198cd2fcffd65fa377e87561424
ers1905 <ers1905@hotmail.com>
2021-05-04 10:46:30 +0300

    Create Soru14.py
    soru 14

---
+++ Ogrenciler/Ersan/Soru14.py
@@ -0,0 +1,11 @@
+print("\nHoşgeldiniz")
+kenar1=int(input("Lütfen saat yönündeki en üst kenarı giriniz: "))
+kenar2=int(input("Lütfen saat yönündeki en sağ kenarı giriniz: "))
+kenar3=int(input("Lütfen saat yönündeki en alt kenarı giriniz: "))
+kenar4=int(input("Lütfen saat yönündeki en sol kenarı giriniz:"))
+if (kenar1 == kenar2):
+    print("Girgiğiniz veriler KARE'yi işaret ediyor")
+else:
+    print("Girgiğiniz veriler DİKDÖRTGEN'i işaret ediyor")
+
+    
