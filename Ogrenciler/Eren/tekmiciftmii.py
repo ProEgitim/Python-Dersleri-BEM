384c9e9b802c2094b940c4d81ecb120555b29521
Eren-123 <63911902+Eren-123@users.noreply.github.com>
2021-05-08 00:04:52 +0300

    Create tekmiciftmii.py
    tekmi çiftmi sorusu yapıldı

---
+++ Ogrenciler/Eren/tekmiciftmii.py
@@ -0,0 +1,8 @@
+#kullanıcıdan alınan bir sayının tek mi ya da çift mi olduğunu bulan kodu yazınız
+
+sayi = int(input("Bir Sayı Giriniz : "))
+
+if sayi%2==0:
+     print(" Sayısı Çift Sayıdır. ",sayi)
+else:
+     print(" Sayısı Tek Sayıdır. ",sayi )
