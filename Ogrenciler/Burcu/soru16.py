cdd8762aabbafde460d059be71b7082fa7430013
Burcu-willow <82774292+Burcu-willow@users.noreply.github.com>
2021-05-07 23:16:27 +0300

    Update soru16.py
    tek mi çift mi

--- Ogrenciler/Burcu/soru16.py
+++ Ogrenciler/Burcu/soru16.py
@@ -1,2 +1,12 @@
-# SORU 16
-Kullanıcıdan alınan bir sayının tek mi ya da çift mi olduğunu bulan kodu yazınız---Burcu-Willow
+"""
+# Kullanıcıdan alınan bir sayının tek mi ya da çift mi olduğunu bulan kodu yazınız---Burcu-Willow
+
+"""
+a=input("bir sayı giriniz")
+if int(a)%2==0:
+    print("bu sayı çiftir.")
+else:
+    print("bu sayı tektir.")
+
+
+
