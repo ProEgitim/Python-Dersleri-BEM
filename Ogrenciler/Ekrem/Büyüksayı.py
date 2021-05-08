1787cc5c0557c9ca54b32edb3f7a19fd2e74a084
Ekrmygt <82719610+Ekrmygt@users.noreply.github.com>
2021-05-08 18:50:55 +0300

    Update Büyüksayı.py

--- Ogrenciler/Ekrem/Büyüksayı.py
+++ Ogrenciler/Ekrem/Büyüksayı.py
@@ -1,25 +1,15 @@
-#Girilen 5 sayıdan en büyüğünü gösteren  programı yazınız.
-
-a=int(input("1.Sayiyi Giriniz: "))
-b=int(input("2.Sayiyi Giriniz: "))
-c=int(input("3.Sayiyi Giriniz: "))
-d=int(input("4.Sayiyi Giriniz: "))
-e=int(input("5.Sayiyi Giriniz: "))
-
-if (a>=b) and (a>=c) and (a>=d) and (a>=e):
-   buyuksayi = a
-   
-elif (b>=a) and (b>=c) and (b>=d) and (b>=e):
-   buyuksayi = b
-elif (c>=a) and (c>=b) and (c>=d) and (c>=e):
-   buyuksayi = c
-elif(d>=a) and (d>=b) and (d>=c) and (d >=e):
-   buyuksayi = d
-else:
-    buyuksayi=e
-print(a,",",b,",",c,",",d,"Ve",e,"içinde büyük olan sayı >>>>>",buyuksayi)
-
-
-
+sayi1 = int(input("1. Sayı: "))
+sayi2 = int(input("2. Sayı: "))
+sayi3 = int(input("3. Sayı: "))
+sayi4 = int(input("4. Sayı: "))
+sayi5 = int(input("5. Sayı: "))
+sayilar=[];
+sayilar.append(sayi1)
+sayilar.append(sayi2)
+sayilar.append(sayi3)
+sayilar.append(sayi4)
+sayilar.append(sayi5)
+sayilar.sort()
+print("En büyük sayimiz..",sayilar[-1])
 
 
