ed9c0a6d0a11d0ebdd6a7b77e6e8ceab79860190
Ekrmygt <82719610+Ekrmygt@users.noreply.github.com>
2021-05-08 18:28:50 +0300

    Update hesapmakinası.py

---
+++ Ogrenciler/Ekrem/hesapmakinası.py
@@ -0,0 +1,15 @@
+sayi1=int(input("1.sayiyi giriniz.."))
+islem=input("işlemi seçiniz + - * / ..")
+sayi2=int(input("2.sayiyi giriniz.."))
+
+
+if islem == "+":
+    sonuc=sayi1+sayi2
+elif islem == "-":
+    sonuc = sayi1-sayi2
+elif islem=="*":
+    sonuc = sayi1*sayi2
+elif islem == "/":
+    sonuc = sayi1/sayi2
+
+print("{}{}{}={}".format(sayi1,islem,sayi2,sonuc))   
