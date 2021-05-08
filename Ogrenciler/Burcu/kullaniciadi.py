3369a2804f2f85ee3c73a670b192084b175bd982
Burcu-willow <82774292+Burcu-willow@users.noreply.github.com>
2021-05-08 19:17:42 +0300

    kullanıcı adı
    hello

---
+++ Ogrenciler/Burcu/kullaniciadi.py
@@ -0,0 +1,19 @@
+print("-----------Kullanıcı Girişi---------")
+
+kullaniciadi="Burcu"
+parola= "256097"
+
+username= input("Kullancı adınızı giriniz: ")
+password= input("Parolanızı giriniz: ")
+
+if username==kullaniciadi and password==parola: 
+    print("tebrikler başarılı giriş yaptınız.")
+
+elif username!=kullaniciadi and password==parola:
+    print("kullanıcıyı adını yanlış girdiniz.")
+
+elif username==kullaniciadi and password!=parola:
+    print("şifreyi yanlış girdiniz.")
+
+elif username!=kullaniciadi and password!=parola:
+    print("kullanıcıyı ve şifreyi yanlış girdiniz.")
--- Ogrenciler/Burcu/soru12.py
+++ Ogrenciler/Burcu/soru12.py
@@ -3,6 +3,6 @@ uzunkenar=int(input("kısa kenarı yazınız:"))
 cevre=2*(int(kisakenar)+int(uzunkenar))
 alan=int(kisakenar)*int(uzunkenar)
 
-print=("\ndikdörtgenin çevresi:{0}".format(cevre))
-print=("\ndikdörtgenin alanı:{0}".format(alan))
+print=("dikdörtgenin çevresi:{0}".format(cevre))
+print=("dikdörtgenin alanı:{0}".format(alan))
 
