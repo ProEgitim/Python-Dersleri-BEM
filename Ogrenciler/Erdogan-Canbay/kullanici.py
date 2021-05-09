40a8052506c7311ba2e87bd41788ddb1740c01f8
Canbay-Erdogan <Canbay-Erdogan@users.noreply.github.com>
2021-05-09 18:23:59 +0300

    kullanici hakkı

--- Ogrenciler/Erdogan-Canbay/kullanici.py
+++ Ogrenciler/Erdogan-Canbay/kullanici.py
@@ -1,11 +1,14 @@
-kullanici=input("kullanici adi giriniz ")
-sifre=input("sifre giriniz ")
-
-if kullanici == "Erdogan":
-    if sifre == "123":
-        print("Hoşgeldiniz..")
-    else:
-        print("Şifre yanlış")
-else:
-    print("hatalı giriş")
+hak=3
+while(hak):
+    kullanici=input("kullanici adi giriniz ")
+    sifre=input("sifre giriniz ")
     
+    if kullanici == "Erdogan":
+        if sifre == "123":
+            print("Hoşgeldiniz..")
+        else:
+            print("Şifre yanlış")
+    else:
+        print("hatalı giriş son hak",hak-1)
+        hak-=1
+print("hakkınız doldu")
