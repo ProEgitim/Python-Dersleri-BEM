12c937723adb63d09bb1b31193ee6cf463ce1e60
ers1905 <ers1905@hotmail.com>
2021-05-09 20:45:06 +0300

    sayı tahmin
    sayı tahmin

--- Ogrenciler/Ersan/sayitahmin.py
+++ Ogrenciler/Ersan/sayitahmin.py
@@ -1,15 +1,24 @@
+from random import randint
 print("""Sayı Tahmin Oyunu
 --------------------------
 """)
-sayi = 17
+
+
+rand = randint (1,100)
+sayac = 0
 
 while True:
-    tahmin=int(input("LÜtfen bir sayı giriniz:"))
-    if (tahmin == sayi):
-        print("Tebrikler doğru tahmin yine bekleriz")
+    sayac += 1
+    sayi=int(input("Lütfen 1 ile 100 arasında bir sayı giriniz: "))
+    if (sayi == 0):
+        print("Oyunu iptal ettiniz")
         break
-    elif(tahmin != sayi):
-        print("Lütfen tahmininizi yenileyin")
-    
-    
-    
+    elif(sayi< rand):
+        print("Lütfen daha yüksek bir sayı giriniz")
+        continue
+    elif (sayi > rand):
+        print("Lütfen daha düşük bir sayı giriniz")
+        continue
+    else:
+        print("Rastgele seçilen sayı {}".format(rand))
+        print("Tahmin sayınız {}".format(sayac))
