d708de2d213d6786be6a6a70e26667a3cb05ca83
ers1905 <ers1905@hotmail.com>
2021-05-10 22:58:32 +0300

    Create carpimtablosu.py
    çarpım tablosu

---
+++ Ogrenciler/Ersan/carpimtablosu.py
@@ -0,0 +1,7 @@
+print("\n---------- Çarpım Tablosu ----------")
+for e in range(1,10):
+    print("""\n---------------------------
+    \n""")
+    
+    for p in range(1,10):
+        print("{} x {} = {}".format(e,p,e*p))
