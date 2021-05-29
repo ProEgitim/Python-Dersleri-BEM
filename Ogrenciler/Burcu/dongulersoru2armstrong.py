68ffe6160d52fa5c3cf9d5f110cb0458f9f74a5a
Burcu-willow <82774292+Burcu-willow@users.noreply.github.com>
2021-05-29 17:43:23 +0300

    armstrong
    olmadı

---
+++ Ogrenciler/Burcu/dongulersoru2armstrong.py
@@ -0,0 +1,12 @@
+sayi=int(input("sayınızı giriniz:"))
+basamak=int(sayi)
+
+toplam=0
+
+for i in basamak
+    sonuc=int(i)**len(basamak)
+
+if (sayi=sonuc):
+    print("Armstrong sayısıdır")
+else:
+    print ("Armstrong sayısı değildir")
