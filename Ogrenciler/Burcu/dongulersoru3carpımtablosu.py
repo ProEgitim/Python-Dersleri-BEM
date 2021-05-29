ce5006199f1966ecde602c718588044e8545436f
Burcu-willow <82774292+Burcu-willow@users.noreply.github.com>
2021-05-29 17:02:17 +0300

    timestable
    çarpım tablosu

---
+++ Ogrenciler/Burcu/dongulersoru3carpımtablosu.py
@@ -0,0 +1,3 @@
+for a in range(1,11):
+    for b in range(1,11):
+        print(a,"x",b,"=",a*b)   
---
+++ Ogrenciler/Burcu/dongulersoru5ucebolunen.py
@@ -0,0 +1,11 @@
+#1'den 100'e kadar olan ve 3e tam bölünen sayıları bulunuz ?
+
+i=1
+while(i<100):
+    print(i)
+    if (i%3==0):
+        break
+    print("i:", i)
+i+=1
+
+
