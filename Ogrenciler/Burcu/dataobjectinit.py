5c2bd70c5093430f5912926ca987e35a7967ba04
Burcu-willow <82774292+Burcu-willow@users.noreply.github.com>
2021-06-13 18:00:52 +0300

    Create dataobjectinit.py
    deneme

---
+++ Ogrenciler/Burcu/dataobjectinit.py
@@ -0,0 +1,15 @@
+class Animal(object):
+
+    def __init__(self, a, b):
+        self.name = a
+        self.age = b 
+
+    def getAge(self):
+        return self.age
+    def getName(self):
+        print(self.Name)
+
+a1 = Animal("dog", 2)
+a2 = Animal("cat", 1)
+a3 = Animal("pig", 4)    
+a4 = Animal("turtle", 100) 
