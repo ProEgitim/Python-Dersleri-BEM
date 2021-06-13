2d77cf708ea21553b38715c1e75b214a658a0bc0
Burcu-willow <82774292+Burcu-willow@users.noreply.github.com>
2021-06-13 16:40:41 +0300

    function data
    deneme

---
+++ Ogrenciler/Burcu/dataobjectfunc.py
@@ -0,0 +1,11 @@
+class Emp(object):
+    age = 30
+    salary = 1600 ## $ cinsinden
+
+    def ageSalaryRatio(self):
+        print("Ratio:", self.age / self.salary)
+        return Emp()
+        
+
+e1 = Emp()
+e1.ageSalaryRatio()
