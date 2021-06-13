c37f1919bbb19ee4b59dc22bbdf838eb406ff8c3
Burcu-willow <82774292+Burcu-willow@users.noreply.github.com>
2021-06-13 15:52:45 +0300

    Update dataobject2.py
    corrected 2

--- Ogrenciler/Burcu/dataobject2.py
+++ Ogrenciler/Burcu/dataobject2.py
@@ -2,10 +2,13 @@ class Square(object):
     side = 6
     area = 0
    
-    def area(self):
+    def area1(self):
         self.area = self.side * self.side
         print("Area:", self.area)
 
 s1 = Square()
 print(s1.side)
-print(s1.area())
+print(s1.area1())
+
+s1.side = 8
+s1.area1()
