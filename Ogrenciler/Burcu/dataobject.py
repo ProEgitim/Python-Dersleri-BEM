c4d25f8d8e35fd5798540bef77e367cc65fe114d
Burcu-willow <82774292+Burcu-willow@users.noreply.github.com>
2021-06-13 15:22:34 +0300

    Create dataobject.py
    deneme

---
+++ Ogrenciler/Burcu/dataobject.py
@@ -0,0 +1,16 @@
+class Candidate:
+    nationality = "German"
+    age = 30
+    gender = "male"
+
+c1 = Candidate()
+
+print(c1.age)
+print(c1.gender)
+print(c1.nationality)
+
+c1.gender = "football"
+
+print(c1.gender)
+
+
