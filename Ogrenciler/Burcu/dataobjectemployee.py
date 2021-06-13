984b024ffd93d77c80809e44c5c15ce391f44959
Burcu-willow <82774292+Burcu-willow@users.noreply.github.com>
2021-06-13 15:10:51 +0300

    classes
    deneme

---
+++ Ogrenciler/Burcu/dataobjectemployee.py
@@ -0,0 +1,7 @@
+"""  classes  """  
+""" ### attributes: age, name, address
+    ### behaviours : capability, competency"""   
+class Employee(object): 
+    pass 
+employee1 = Employee()
+print(employee1.__class__)
