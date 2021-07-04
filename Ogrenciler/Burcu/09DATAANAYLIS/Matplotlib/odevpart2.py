19b5620abf8338b9bf15ba1a1b8ae2982c7bc35d
Burcu-willow <82774292+Burcu-willow@users.noreply.github.com>
2021-07-04 17:07:52 +0300

    odev2
    changed

--- Ogrenciler/Burcu/09DATAANAYLIS/Matplotlib/odevpart2.py
+++ Ogrenciler/Burcu/09DATAANAYLIS/Matplotlib/odevpart2.py
@@ -15,9 +15,9 @@ axes2=fig.add_axes([0.1,0.5,0.4,0.3])
 plt.title("outherGraph - X vs Z Graph")
 plt.title("innerGraph - X vs Y Graph")
 
-plt.subplot(2,2,1)
+plt.plot(2,2,1)
 plt.plot(x,z,"purple")
-plt.subplot(2,2,2)
+plt.plot(2,2,2)
 plt.plot(x,y,"red")
 
 plt.show()
